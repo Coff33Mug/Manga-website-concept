1574e7a4b6ee1117258bf5bdb9f6991f8a33defb
Coff33Mug <HuynhD2005@gmail.com>
2025-04-06 23:41:15 -0700

    Filter button now functional.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const filterElement = document.getElementById('filterElement');
-let filterElementState = 0;
+document.addEventListener("DOMContentLoaded", () => {
+    const filterElement = document.getElementById("filterElement");
+    let filterElementState = 0;
 
-filterElement.addEventListener("click", function() {
-    // Hidden state -> unhidden
-    if (filterElementState == 0) {
-        filterElement.className = "filterElement";
-        filterElementState = 1;
-    } else {
-        // Unhidden -> hidden
-        filterElement.className = "filterElementHidden";
-        filterElementState = 0;
-    }
+    document.getElementById("filterButton").addEventListener('click', () => {
+        // Hidden state -> unhidden
+        if (filterElementState == 0) {
+            filterElement.className = "filterElement";
+            filterElementState = 1;
+            console.log("first");
+        } else {
+            // Unhidden -> hidden
+            filterElement.className = "filterElement hidden";
+            filterElementState = 0;
+            console.log("second");
+        }
 
+    });
 });
