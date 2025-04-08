cf4da976fe199875af8f3b8434186c11dacbfe58
Coff33Mug <HuynhD2005@gmail.com>
2025-04-07 23:38:06 -0700

    Added account drop down when screen is small.

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const filterElement = document.getElementById("filterElement");
-    let filterElementState = 0;
+    const accountElementDropDown = document.getElementById("accountElementDropDown");
 
     document.getElementById("filterButton").addEventListener('click', () => {
         // Hidden state -> unhidden
-        if (filterElementState == 0) {
-            filterElement.className = "filterElement";
-            filterElementState = 1;
-            console.log("first");
-        } else {
-            // Unhidden -> hidden
-            filterElement.className = "filterElement hidden";
-            filterElementState = 0;
-            console.log("second");
-        }
+        filterElement.classList.toggle("hidden");
+    });
 
+    document.getElementById("accountProfilePictureButton").addEventListener('click', () => {
+        // Hidden state -> unhidden
+        console.log(accountElementDropDown);
+        accountElementDropDown.classList.toggle("hidden");
     });
 });
